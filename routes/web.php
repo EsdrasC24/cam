63012ebc4529b64ff7885471002449f9d79829fc
EsdrasC24 <josuequeraleswd2420@gmail.com>
2023-11-22 11:57:12 -0400

    fix: add routes for models

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,36 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+       //  categories
+       Route::controller(\App\Http\Controllers\CategoryController::class)->group(function (){
+        Route::get('/categorias', 'index')->name('category.index');
+        Route::get('/categorias/crear', 'create')->name('category.create');
+        Route::get('/categoria/{category}/editar', 'edit')->name('category.dit');
+        Route::post('/categoria', 'store')->name('category.store');
+        Route::put('/categoria/{category}', 'update')->name('category.update');
+        Route::delete('/categoria/{category}', 'destroy')->name('category.delete');
+    });
+
+    //  products
+    Route::controller(\App\Http\Controllers\CategoryController::class)->group(function (){
+        Route::get('/productos', 'index')->name('product.index');
+        Route::get('/producto/crear', 'create')->name('product.create');
+        Route::get('/producto/{product}/editar', 'edit')->name('product.dit');
+        Route::post('/producto', 'store')->name('product.store');
+        Route::put('/producto/{product}', 'update')->name('product.update');
+        Route::delete('/producto/{product}', 'destroy')->name('product.delete');
+    });
+
+    //  dayly books
+    Route::controller(\App\Http\Controllers\DaylyBookController::class)->group(function (){
+        Route::get('/libro-diarios', 'index')->name('daylybook.index');
+        Route::get('/libro-diario/crear', 'create')->name('daylybook.create');
+        Route::get('/libro-diario/{daylybook}/editar', 'edit')->name('daylybook.dit');
+        Route::post('/libro-diario', 'store')->name('daylybook.store');
+        Route::put('/libro-diario/{daylybook}', 'update')->name('daylybook.update');
+        Route::delete('/libro-diario/{daylybook}', 'destroy')->name('daylybook.delete');
+    });
 });
 
 require __DIR__.'/auth.php';
