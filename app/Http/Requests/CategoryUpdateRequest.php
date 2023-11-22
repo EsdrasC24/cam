547acded6e7dfdc005818db623213ceaac2567cf
EsdrasC24 <josuequeraleswd2420@gmail.com>
2023-11-22 11:26:25 -0400

    feat: category's process and validations

---
+++ app/Http/Requests/CategoryUpdateRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CategoryUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'name' => ['bail', 'required', 'min:3', Rule::unique('categories', 'name')->ignore($this->args('id'))],
+            'description' => ['nullable', 'string', 'max:150']
+        ];
+    }
+}
