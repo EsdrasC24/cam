712e95639ed242b0d1e92d60acb39e9e32515b44
EsdrasC24 <josuequeraleswd2420@gmail.com>
2023-11-23 10:11:15 -0400

    feat: middleware for check if admin is registered

---
+++ app/Http/Middleware/EnsureIfThereIsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+
+class EnsureIfThereIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if(!User::where('role', 'admin')->count() && !$request->routeIs('register')){
+            return redirect()->route('register');
+        }
+
+        return $next($request);
+    }
+}
